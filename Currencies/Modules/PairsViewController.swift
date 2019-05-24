c0ab91626725b58f08977e826014a37de4980feb
Artem Lytkin <a.lytkin@netris.ru>
2019-05-24 13:26:49 +0300

    Added UI elements into PairsVC

---
+++ Currencies/Modules/PairsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currencies
+//
+//  Created by Artem Lytkin on 24/05/2019.
+//  Copyright © 2019 Artem Lytkin. All rights reserved.
+//
+
+import UIKit
+
+class PairsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
