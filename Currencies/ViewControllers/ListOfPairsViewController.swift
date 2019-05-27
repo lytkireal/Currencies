8cd132a33ab26ec7dbf4b7e73deaab0c950ddcbf
Artem Lytkin <a.lytkin@netris.ru>
2019-05-27 16:00:59 +0300

    Added view controller for list of compared currency pairs

---
+++ Currencies/ViewControllers/ListOfPairsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ListOfPairsViewController.swift
+//  Currencies
+//
+//  Created by Artem Lytkin on 27/05/2019.
+//  Copyright © 2019 Artem Lytkin. All rights reserved.
+//
+
+import UIKit
+
+class ListOfPairsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TopCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
